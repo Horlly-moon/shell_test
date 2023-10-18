d18712b5ad33ccab844a2ace72627d146a83d7b2
Horlly Moon <moonhorlly@gmail.com>
2023-10-18 19:46:04 +0100

    Update builtin1.c

--- builtin1.c
+++ builtin1.c
@@ -1,13 +1,13 @@
 #include "shell.h"
 
 /**
- * _myHistory - displays the history list, one command by line, preceded
+ * _myownHistory - displays the history list, one command by line, preceded
  *              with line numbers, starting at 0.
  * @info: Structure containing potential arguments. Used to maintain
  *        constant function prototype.
  *  Return: Always 0
  */
-int _myHistory(info_t *info)
+int _myownHistory(info_t *info)
 {
 	Print_list(info->history);
 	return (0);
